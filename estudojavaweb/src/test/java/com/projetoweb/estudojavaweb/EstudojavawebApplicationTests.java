30946c1987e47b7aa481b7ed8c43551e4b0f314b
PabloZalem <zalem.pablo@yahoo.com>
2023-08-24 21:03:23 -0300

    Criacao de user e userResources

---
+++ estudojavaweb/src/test/java/com/projetoweb/estudojavaweb/EstudojavawebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoweb.estudojavaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudojavawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
